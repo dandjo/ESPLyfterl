1f978063d2b944a33191fc2d192bfb2fe755dc8f
Daniel Pernold <d.pernold@cyledge.com>
2022-10-04 12:55:16 +0200

    Implemented usage of WiFi reconnect flag

--- src/main.cpp
+++ src/main.cpp
@@ -32,19 +32,6 @@ bool arduino_ota_busy = false;
 #endif
 
 
-void checkWifi() {
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    if (i++ == 120) {
-      Serial.printf("Tried connecting for 60 sec, rebooting now. ");
-      restart_board();
-    }
-  }
-}
-
-
 void setupWifi() {
   delay(10);
   // we start by connecting to a WiFi network
@@ -71,7 +58,16 @@ void setupWifi() {
     }
   #endif
   WiFi.begin(WIFI_SSID, WIFI_PWD);
-  checkWifi();
+  WiFi.setAutoReconnect(true);
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i++ == 120) {
+      Serial.printf("Tried connecting for 60 sec, rebooting now. ");
+      restart_board();
+    }
+  }
   mqttSerial.printf("Connected. IP Address: %s. ", WiFi.localIP().toString().c_str());
 }
 
@@ -139,9 +135,6 @@ void setup() {
 
 void loop() {
   unsigned long start = millis();
-  if (WiFi.status() != WL_CONNECTED) {
-    checkWifi();
-  }
   if (!client.connected()) { // (re)connect to MQTT if needed
     reconnectMqtt();
   }
